96a7c2b4cc2bd9bceb1adccd84c74797dfa81783
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-05 17:49:37 +0200

    add: footer menus array

--- config/menus.php
+++ config/menus.php
@@ -42,5 +42,145 @@ return [
       'text' => "shop",
       'href' => "shop"
     ]
+  ],
+
+  'footer_menu' =>   [
+    [
+      'dc Comic' => [
+        [
+          'text' => "characters",
+          'href' => "#"
+        ],
+        [
+          'text' => "comics",
+          'href' => "#"
+        ],
+        [
+          'text' => "movies",
+          'href' => "#"
+        ],
+        [
+          'text' => "TV",
+          'href' => "#"
+        ],
+        [
+          'text' => "games",
+          'href' => "#"
+        ],
+        [
+          'text' => "videos",
+          'href' => "#"
+        ],
+        [
+          'text' => "news",
+          'href' => "#"
+        ]
+      ],
+      'shop' => [
+        [
+          'text' => "shop DC",
+          'href' => "#"
+        ],
+        [
+          'text' => "shop DC collectibles",
+          'href' => "#"
+        ]
+      ]
+    ],
+    [
+      'dc' => [
+        [
+          'text' => "terms of use",
+          'href' => "#"
+        ],
+        [
+          'text' => "privacy policy (new)",
+          'href' => "#"
+        ],
+        [
+          'text' => "sd choices",
+          'href' => "#"
+        ],
+        [
+          'text' => "advertising",
+          'href' => "#"
+        ],
+        [
+          'text' => "jobs",
+          'href' => "#"
+        ],
+        [
+          'text' => "subscriptions",
+          'href' => "#"
+        ],
+        [
+          'text' => "talent workshops",
+          'href' => "#"
+        ],
+        [
+          'text' => "CPSC certificates",
+          'href' => "#"
+        ],
+        [
+          'text' => "ratings",
+          'href' => "#"
+        ],
+        [
+          'text' => "shop help",
+          'href' => "#"
+        ],
+        [
+          'text' => "contact us",
+          'href' => "#"
+        ]
+      ],
+    ],
+    [
+    'sites' => [
+      [
+        'text' => "DC",
+        'href' => "#"
+      ],
+      [
+        'text' => "MAD magazine",
+        'href' => "#"
+      ],
+      [
+        'text' => "DC kids",
+        'href' => "#"
+      ],
+      [
+        'text' => "DC universe",
+        'href' => "#"
+      ],
+      [
+        'text' => "DC power visa",
+        'href' => "#"
+      ],
+      ]
+    ]
+  ],
+
+  'social_menu' => [
+    [
+      'img' => "/resources/img/footer-facebook.png",
+      'href' => "#"
+    ],
+    [
+      'img' => "/resources/img/footer-twitter.png",
+      'href' => "#"
+    ],
+    [
+      'img' => "/resources/img/footer-youtube.png",
+      'href' => "#"
+    ],
+    [
+      'img' => "/resources/img/footer-pinterest.png",
+      'href' => "#"
+    ],
+    [
+      'img' => "/resources/img/footer-periscope.png",
+      'href' => "#"
+    ]
   ]
 ];
