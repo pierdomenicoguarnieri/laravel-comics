c2d97e7650ecef6d3443e8f4fbf7fb36ae0062c0
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-05 16:47:37 +0200

    add: header markup and cycle using foreach

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+@php
+  $menu_header = config('menus.header_menu');
+@endphp
+
+<header>
+  <div class="container">
+    <div class="left">
+      <a href="{{route('home')}}"><img src="/img/dc-logo.png" alt="DC Comics Logo"></a>
+    </div>
+    <div class="right">
+      <nav>
+        <ul>
+          @foreach ($menu_header as $link)
+          <li><a href="{{route($link['href'])}}" class="text-uppercase {{Route::currentRouteName() === $link['href'] ? 'active' : ''}}">{{$link['text']}}</a></li>
+          @endforeach
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
