9642da4f8f0311cf7c47e2dc0bb957c8a94c8a05
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-05 16:44:56 +0200

    add: menus.php array

---
+++ config/menus.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+  'header_menu' => [
+    [
+      'text' => "characters",
+      'href' => "home"
+    ],
+    [
+      'text' => "comics",
+      'href' => "comics"
+    ],
+    [
+      'text' => "movies",
+      'href' => "movies"
+    ],
+    [
+      'text' => "tv",
+      'href' => "tv"
+    ],
+    [
+      'text' => "games",
+      'href' => "games"
+    ],
+    [
+      'text' => "collectibles",
+      'href' => "collectibles"
+    ],
+    [
+      'text' => "videos",
+      'href' => "videos"
+    ],
+    [
+      'text' => "fans",
+      'href' => "fans"
+    ],
+    [
+      'text' => "news",
+      'href' => "news"
+    ],
+    [
+      'text' => "shop",
+      'href' => "shop"
+    ]
+  ]
+];
