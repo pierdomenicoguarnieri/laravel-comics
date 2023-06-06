3d2c60cabc30df1c58026a8085f4777020e18e09
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-06 20:09:43 +0200

    add: back to home button in comic-info

--- resources/views/comic-info.blade.php
+++ resources/views/comic-info.blade.php
@@ -7,15 +7,21 @@
 @section('content')
   <main>
     <div class="pg-main-wrapper">
+
       <div class="pg-img-wrapper">
+
         <div class="pg-img-container">
           <img src="{{$comic['thumb']}}" alt="">
         </div>
       </div>
       <div class="container">
+
         <div class="pg-upper-content py-5">
+
           <div class="row">
+
             <div class="col col-8 flex-column">
+
               <h1 class="text-uppercase">{{$comic['title']}}</h1>
 
               <div class="pg-price-container border-bottom border-black mt-3">
@@ -23,6 +29,7 @@
                   <span class="ms-4">U.S. Price: {{$comic['price']}}</span>
                   <span class="text-uppercase me-4">available</span>
                 </div>
+
                 <div class="right h-100 text-center border-start border-black py-3">
                   <span class="text-capitalize">check availability</span>
                 </div>
@@ -42,14 +49,18 @@
       <div class="pg-lower-content py-5 border-top border-dark-subtle bg-light">
         <div class="container">
           <div class="row">
+
             <div class="col d-flex flex-column text-start">
               <h4>Talent</h4>
+
               <table class="table mt-3">
+
                 <tbody>
                   <tr class="table-light">
                     <td class="w-25">Art by:</td>
                     <td>{{implode(", ", $comic['artists'])}}</td>
                   </tr>
+
                   <tr class="table-light">
                     <td class="w-25">Written by:</td>
                     <td>{{implode(", ", $comic['writers'])}}</td>
@@ -57,18 +68,24 @@
                 </tbody>
               </table>
             </div>
+
             <div class="col d-flex flex-column text-start">
+
               <h4>Specs</h4>
+
               <table class="table mt-3">
+
                 <tbody>
                   <tr class="table-light">
                     <td class="w-25">Series:</td>
                     <td>{{$comic['series']}}</td>
                   </tr>
+
                   <tr class="table-light">
                     <td class="w-25">U.S. Price:</td>
                     <td>{{$comic['price']}}</td>
                   </tr>
+
                   <tr class="table-light">
                     <td class="text-capitalize w-25">on sale date:</td>
                     <td>{{$comic['sale_date']}}</td>
@@ -76,6 +93,10 @@
                 </tbody>
               </table>
             </div>
+          </div>
+
+          <div class="pg-button-container w-100 mt-5">
+            <a href="{{route('comics')}}"><button type="button" class="btn bt-primary">Torna alla home</button></a>
           </div>
         </div>
       </div>
