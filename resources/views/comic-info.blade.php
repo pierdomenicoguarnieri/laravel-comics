49338ce226ef141530b05f452b2e825c0b3cf1a5
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-06 20:00:19 +0200

    add: table and comics infos

--- resources/views/comic-info.blade.php
+++ resources/views/comic-info.blade.php
@@ -28,7 +28,7 @@
                 </div>
               </div>
 
-              <p class="mt-3 fs-5">{{$comic['description']}}</p>
+              <p class="mt-3 text-muted">{{$comic['description']}}</p>
             </div>
 
             <div class="col col-4 flex-column text-end text-uppercase">
@@ -38,8 +38,45 @@
           </div>
         </div>
 
-        <div class="pg-lower-content">
-
+      </div>
+      <div class="pg-lower-content py-5 border-top border-dark-subtle bg-light">
+        <div class="container">
+          <div class="row">
+            <div class="col d-flex flex-column text-start">
+              <h4>Talent</h4>
+              <table class="table mt-3">
+                <tbody>
+                  <tr class="table-light">
+                    <td class="w-25">Art by:</td>
+                    <td>{{implode(", ", $comic['artists'])}}</td>
+                  </tr>
+                  <tr class="table-light">
+                    <td class="w-25">Written by:</td>
+                    <td>{{implode(", ", $comic['writers'])}}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="col d-flex flex-column text-start">
+              <h4>Specs</h4>
+              <table class="table mt-3">
+                <tbody>
+                  <tr class="table-light">
+                    <td class="w-25">Series:</td>
+                    <td>{{$comic['series']}}</td>
+                  </tr>
+                  <tr class="table-light">
+                    <td class="w-25">U.S. Price:</td>
+                    <td>{{$comic['price']}}</td>
+                  </tr>
+                  <tr class="table-light">
+                    <td class="text-capitalize w-25">on sale date:</td>
+                    <td>{{$comic['sale_date']}}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
